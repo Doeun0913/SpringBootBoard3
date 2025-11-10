22794e55f6ed9654f8d663567720d9fc19003fb2
Doeun <lde28700409@gmail.com>
2025-11-10 15:29:48 +0900

    사용자 이름 등록

--- sbb/src/main/java/com/mysite/sbb/question/Question.java
+++ sbb/src/main/java/com/mysite/sbb/question/Question.java
@@ -4,13 +4,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.user.SiteUser;
 
 import jakarta.persistence.CascadeType; 
 import jakarta.persistence.Column; 
 import jakarta.persistence.Entity; 
 import jakarta.persistence.GeneratedValue; 
 import jakarta.persistence.GenerationType; 
-import jakarta.persistence.Id; 
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany; 
 
 import lombok.Getter; 
@@ -33,5 +35,8 @@ public class Question {
     private LocalDateTime createDate; 
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE) 
-    private List<Answer> answerList; 
+    private List<Answer> answerList;
+    
+    @ManyToOne
+    private SiteUser author;
 }
