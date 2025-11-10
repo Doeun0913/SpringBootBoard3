b5eb02a62017a117b5cd2c88fc65038a58f7164a
Doeun <lde28700409@gmail.com>
2025-11-10 18:13:03 +0900

    sbb서버 사용

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -21,7 +21,9 @@ import com.mysite.sbb.user.UserService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
@@ -31,8 +33,10 @@ public class QuestionController {
     private final UserService userService;
 
     @GetMapping("/list")
-    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
-        Page<Question> paging = this.questionService.getList(page);
+    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page,
+    		@RequestParam(value = "kw", defaultValue = "") String kw) {
+    	log.info("page:{}, kw:{}", page, kw);
+        Page<Question> paging = this.questionService.getList(page, kw);
         model.addAttribute("paging", paging);
         return "question_list";
     }
