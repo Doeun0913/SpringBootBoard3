e4ad2f2c13c7e1aa79cb26a896bf66bdc7a98321
Doeun <lde28700409@gmail.com>
2025-11-10 10:17:53 +0900

    HelloLombok 수정

--- sbb/src/main/java/com/mysite/sbb/HelloLombok.java
+++ sbb/src/main/java/com/mysite/sbb/HelloLombok.java
@@ -1,35 +1,17 @@
 package com.mysite.sbb;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+@RequiredArgsConstructor
 @Getter
-@Setter
 public class HelloLombok {
-	private String hello;
-	private int lombok;
-	
-	public void setHello(String hello) {
-		this.hello = hello;
-	}
-	
-	public void setLombok(int lombok) {
-		this.lombok = lombok;
-	}
-	
-	public String getHello() {
-		return this.hello;
-	}
-	
-	public int getLombok() {
-		return this.lombok;
-	}
-	
+	private final String hello;
+	private final int lombok;
+
 	public static void main(String[] args) {
-		HelloLombok helloLombok = new HelloLombok();
-		helloLombok.setHello("헬로");
-		helloLombok.setLombok(5);
-		
+		HelloLombok helloLombok = new HelloLombok("헬로", 5);
 		System.out.println(helloLombok.getHello());
 		System.out.println(helloLombok.getLombok());
 	}
